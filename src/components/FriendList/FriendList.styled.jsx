fd18445f07f57056ed094cd263d11808f764b8c8
Андрій <122921284+AndrijAyzy@users.noreply.github.com>
2024-02-06 20:09:54 +0200

    dfg

--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -20,8 +20,7 @@ export const ListItem = styled.li`
   margin: 0px auto;
   padding: 10px;
   border-radius: 5px;
-  box-shadow: 0px 1px 3px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 14%),
-    0px 2px 1px rgb(0 0 0 / 20%);
+ 
   :not(:last-child) {
     margin-bottom: 20px;
   }
